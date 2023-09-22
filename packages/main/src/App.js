c644d1a75805dc211a84ba7b5f8448054d0e675a
Pikalo-dragonballz <145779998+Pikalo-dragonballz@users.noreply.github.com>
2023-09-22 19:16:51 +0530

    Add files via upload

---
+++ packages/main/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Product from "@monorepo/product";
+
+const App = () => {
+  return (
+    <>
+       <h1>Hello world! I am using React</h1>
+       <Product />
+    </>
+  );
+};
+
+export default App;
---
+++ packages/product/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Product = () => {
+  return <h1>Hello from the product package</h1>;
+};
+
+export default Product;
